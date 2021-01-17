c7f3075bb1faa70127bee3de4c59e398e124ebeb
reitnue <29718700+reitnue@users.noreply.github.com>
2021-01-17 15:39:54 -0500

    Add Tap Dance (#3)
    * Add tap dance, ii to ESC
    
    * Add num pad and remove caps to esc

--- keyboards/massdrop/alt/keymaps/reitnue/keymap.c
+++ keyboards/massdrop/alt/keymaps/reitnue/keymap.c
@@ -10,20 +10,28 @@ enum alt_keycodes {
     MD_BOOT,               //Restart into bootloader after hold timeout
 };
 
+enum tap_dance_declarations {
+    TD_I_ESC,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_I_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_I, KC_ESC), // tap once for "i", twitce for ESC
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
-        KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-        LT(2, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
-        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-        KC_LCTL,        KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,            KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    TD(TD_I_ESC),    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
+        MO(2),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,            KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
+        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,         KC_DOT,  KC_SLSH, MO(3),            KC_UP,   KC_PGDN, \
+        KC_LCTL,        KC_LALT, KC_LGUI,                            KC_SPC,                            KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
-        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END, \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END,  \
         _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
         _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, _______,          KC_PGUP, KC_VOLD, \
-        _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END  \
+        _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END   \
     ),
     [2] = LAYOUT_65_ansi_blocker(
         _______, KC_BRID, KC_BRIU, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, \
@@ -31,6 +39,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
         _______, LAG_SWP, LAG_NRM,                            _______,                            _______, _______, _______, _______, _______  \
+    ),
+    [3] = LAYOUT_65_ansi_blocker(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, KC_4,    KC_5,    KC_6,    _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______,                            KC_0,                               _______, _______, _______, _______, _______  \
     ),
     /*
     [X] = LAYOUT(
@@ -123,14 +138,16 @@ void rgb_matrix_indicators_user(void) {
     }
     int layer = get_highest_layer(layer_state);
     switch (layer) {
+        case 3:
+            // explicit fallthrough
         case 2:
             rgb_matrix_set_color(36, RGB_WHITE);
             rgb_matrix_set_color(37, RGB_WHITE);
             rgb_matrix_set_color(38, RGB_WHITE);
             rgb_matrix_set_color(39, RGB_WHITE);
-            // explicty fallthrough
+            // explicit fallthrough
         case 1:
-            // explicty fallthrough
+            // explicit fallthrough
         case 0:
             rgb_matrix_set_color(1, RGB_MAGENTA);
             rgb_matrix_set_color(2, RGB_GREEN);
@@ -142,7 +159,7 @@ void rgb_matrix_indicators_user(void) {
             rgb_matrix_set_color(8, RGB_GREEN);
             rgb_matrix_set_color(9, RGB_MAGENTA);
             rgb_matrix_set_color(10, RGB_GREEN);
-            // explicty fallthrough
+            // explicit fallthrough
         default:
             break;
     }
--- keyboards/massdrop/alt/rules.mk
+++ keyboards/massdrop/alt/rules.mk
@@ -30,3 +30,5 @@ RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = custom
 
 LAYOUTS = 65_ansi_blocker
+
+TAP_DANCE_ENABLE = yes
