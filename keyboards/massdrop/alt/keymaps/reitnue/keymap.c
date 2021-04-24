90abee17c9c84c19550138b57d65d3ef59cb9d0b
Alex Wang <reitnue@users.noreply.github.com>
2021-04-24 16:23:27 -0400

    Merge reitnue

--- keyboards/massdrop/alt/keymaps/reitnue/keymap.c
+++ keyboards/massdrop/alt/keymaps/reitnue/keymap.c
@@ -12,7 +12,6 @@ enum alt_keycodes {
 
 enum tap_dance_declarations {
     TD_I_ESC,
-    TD_A_CTRL_A,
     TD_C_CTRL_C,
     TD_Z_CTRL_Z,
 };
@@ -33,9 +32,6 @@ static td_state_t td_state;
 uint8_t cur_dance(qk_tap_dance_state_t *state);
 
 // `finished` and `reset` functions for each tapdance keycode
-// TD_A_CTRL_A
-void td_a_ctrl_a_finished(qk_tap_dance_state_t *state, void *user_data);
-void td_a_ctrl_a_reset(qk_tap_dance_state_t *state, void *user_data);
 // TD_C_CTRL_C
 void td_c_ctrl_c_finished(qk_tap_dance_state_t *state, void *user_data);
 void td_c_ctrl_c_reset(qk_tap_dance_state_t *state, void *user_data);
@@ -46,7 +42,6 @@ void td_z_ctrl_z_reset(qk_tap_dance_state_t *state, void *user_data);
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_I_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_I, KC_ESC), // tap once for "i", twitce for ESC
-    [TD_A_CTRL_A] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_a_ctrl_a_finished, td_a_ctrl_a_reset), // tap once for "a", twice fo Ctrl a
     [TD_C_CTRL_C] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_c_ctrl_c_finished, td_c_ctrl_c_reset), // tap once for "a", twice fo Ctrl a
     [TD_Z_CTRL_Z] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_z_ctrl_z_finished, td_z_ctrl_z_reset), // tap once for "a", twice fo Ctrl a
 };
@@ -55,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
         KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,            KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,         LCTL_T(KC_Q),    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    TD(TD_I_ESC),    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-        MO(2),          TD(TD_A_CTRL_A),    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,            KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
+        MO(2),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,            KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT,        TD(TD_Z_CTRL_Z),    KC_X,    TD(TD_C_CTRL_C),    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,         KC_DOT,  KC_SLSH, MO(3),            KC_UP,   KC_PGDN, \
         KC_LCTL,        KC_LALT, KC_LGUI,                            KC_SPC,                            KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
@@ -215,28 +210,6 @@ uint8_t cur_dance(qk_tap_dance_state_t *state) {
 }
 
 // Handle the possible states for each tapdance keycode you define:
-// TD_A_CTRL_A
-void td_a_ctrl_a_finished(qk_tap_dance_state_t *state, void *user_data) {
-    td_state = cur_dance(state);
-    switch (td_state) {
-        case SINGLE_TAP:
-            tap_code(KC_A);
-            break;
-        case DOUBLE_SINGLE_TAP:
-            register_code16(LCTL(KC_A));
-    }
-}
-
-void td_a_ctrl_a_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (td_state) {
-        case SINGLE_TAP:
-            break;
-        case DOUBLE_SINGLE_TAP:
-            unregister_code16(LCTL(KC_A));
-    }
-}
-
-
 // TD_C_CTRL_C
 void td_c_ctrl_c_finished(qk_tap_dance_state_t *state, void *user_data) {
     td_state = cur_dance(state);
