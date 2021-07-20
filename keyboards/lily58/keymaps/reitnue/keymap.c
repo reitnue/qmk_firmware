ae7ff74073deecb399c001738341859beebde5b3
Alex Wang <reitnue@users.noreply.github.com>
2021-07-20 10:02:18 -0400

    Swap lower and lctrl

--- keyboards/lily58/keymaps/reitnue/keymap.c
+++ keyboards/lily58/keymaps/reitnue/keymap.c
@@ -23,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+--------+------+---------------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  | TD(I, Esacpe) |   O  |   P  |  -     |
  * |------+------+------+------+------+------|                    |------+------+---------------+------+------+--------|
- * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K           |   L  |   ;  |  '     |
+ * | LOWER |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K           |   L  |   ;  |  '     |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+---------------+------+------+--------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,           |   .  |   /  | RShift |
  * `-----------------------------------------/       /     \      \----------------------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   | LAlt | LGUI | LCTRL | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_GRAVE,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPACE, \
   KC_TAB,   LCTL_T(KC_Q),   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    TD(TD_I_ESC),    KC_O,    KC_P,    KC_MINS, \
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  MO(_LOWER), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
+                        KC_LGUI, KC_LALT, KC_LCTRL, KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
